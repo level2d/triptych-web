dfc88625d56820bb454ece91e501c4f1418a9ed4
Tyler Johnson <tyler@level2d.com>
2025-08-29 08:44:16 -0400

    trying to get the stuff to work

--- embedded_1.js
+++ embedded_1.js
@@ -1,3 +1,5 @@
+console.log('Embedded script loaded');
+
 // Create a new div and insert it before the closing </body> tag
 var newDiv = document.createElement('div');
 newDiv.id = 'custom-embed-div'; // Optional: set an ID
